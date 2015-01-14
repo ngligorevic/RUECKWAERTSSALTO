cf1b73360755421c0988605c7b39768b95b5a1f3
ngligorevic <ngligorevic@student.tgm.ac.at>
2015-01-14 10:54:21 +0100

    Docomentations added

--- src/Connection.java
+++ src/Connection.java
@@ -8,10 +8,30 @@ import java.util.ArrayList;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
+/*
+ * 
+ * Aufbau einer Connection zu einer RDBMS und 
+ * deren Verwaltung
+ * @author Nenad Gligorevic
+ * @author Melanie Goebel
+ * @version 20140114
+ * 
+ */
 public class Connection {
 	private java.sql.Connection con;
 	private MysqlDataSource ds;
 	
+	/**
+	 * 
+	 * @param hostname Adresse des Hosts
+	 * @param user Name des Users
+	 * @param password Passwort des Users
+	 * 
+     * Methode kümmert sich um die Verbindung mit der Datenbank
+	 * Falls Host, DB, Pass oder DB name nicht angegeben werden
+	 * so wird dem entsprechend darauf mit Exception und Fehlermeldeungen
+	 * reagiert
+	 */
 	public void connect (String hostname,String user,String password){
 		ds = new MysqlDataSource();
 		ds.setServerName(hostname);
@@ -23,6 +43,9 @@ public class Connection {
 			System.err.println("Failed to connect to "+hostname);
 		}
 	}
+	/**
+	 * Schliesst die Verbindung zur Datenbank
+	 */
 	public void close(){
 		try {
 			con.close();
@@ -30,6 +53,14 @@ public class Connection {
 			System.err.println("Failed to close connection to "+ds.getServerName());
 		}
 	}
+	/**
+	 * 
+	 * @param database die Datenbank
+	 * @return Tabellen der Datenbank
+	 * 
+	 * Tabellen werden in ArrayList gespeichert 
+	 * und ausgelesen. 
+	 */
 	public ArrayList<String> getTables(String database){
 		Statement st;
 		ArrayList<String> tables = new ArrayList<String>();
