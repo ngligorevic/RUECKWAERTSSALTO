b1e3b4ec8aaf6ea57e2af0e5c806103128d345ff
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2015-01-21 10:46:35 +0100

    Output in file

--- src/Diagram.java
+++ src/Diagram.java
@@ -1,5 +1,10 @@
 package rueckwaertssalto;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 /**
  * @author Melanie Goebel
@@ -8,31 +13,47 @@ import java.util.ArrayList;
  */
 public class Diagram{
 
-	public String getRM(Connection con, String database){
-		String ausg = "";
+	public boolean getRM(Connection con, String database, String filename){
+		File file;
+		FileWriter rf;
+		BufferedWriter bf;
+		try{
+		file = new File(filename);
+		rf = new FileWriter(file);
+		bf = new BufferedWriter(rf); 
 		ArrayList<String> atr;
 		ArrayList<String> pk;
 		ArrayList<ForeignKey> fk;
 		ArrayList<Tabelle> tabellen = con.getTables(database);
 		for(int i = 0; i < tabellen.size(); i++){
-			ausg += tabellen.get(i).getName()+"(";
+			bf.write(tabellen.get(i).getName()+"(");
 			con.getInfo(tabellen.get(i));
 			atr = tabellen.get(i).getAttributs();
 			pk = tabellen.get(i).getPrimarykeys();
 			fk = tabellen.get(i).getForeignkeys();
 			for(int j = 0; j < atr.size(); j++){
 				if(pk.contains(atr.get(j)))
-					ausg += "<PK>";
+					bf.write("<PK>");
 				String table = getFKTable(fk,atr.get(j));
 				if(table != null)
-					ausg += "<FK>"+table+".";
-				ausg += atr.get(j);
+					bf.write("<FK>"+table+".");
+				bf.write( atr.get(j));
 				if(j < atr.size()-1)
-					ausg += ", ";
+					bf.write(", ");
 			}
-			ausg += ")\n";
+			bf.write(")");
+			bf.newLine();//Zeilenumbruch
 		}
-		return ausg;
+		bf.close();rf.close();//Schließen damit man es nochmal verwenden kann.
+		} catch (FileNotFoundException ex) {
+			System.err.println("File "+filename+" cannot be created");
+			return false;
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			System.err.println("Failed to write in "+filename);
+			return false;
+		}
+		return true;
 	}
 	private String getFKTable(ArrayList<ForeignKey> fk, String key){
 		for(int i=0; i < fk.size(); i++){
--- src/Start.java
+++ src/Start.java
@@ -1,13 +1,11 @@
 package rueckwaertssalto;
 
-import java.util.ArrayList;
-
 public class Start {
 	public static void main(String[] args) {
 		Connection con = new Connection();
-		con.connect("10.0.0.8", "insy4", "blabla");
+		con.connect("10.0.105.60", "insy4", "blabla");
 		Diagram d = new Diagram();
-		System.out.println(d.getRM(con, "testinsy"));
+		System.out.println(d.getRM(con, "testinsy","rm.txt"));
 		
 	}
 }
